ca52d0713aca141612d770bad726f3d91a855104
Guillaume ALAMOME <guillaume.alamome@gmail.com>
2025-09-15 15:44:39 +0200

    Apply transform and withField

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 package com.galamome
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions.{col, lit, transform}
 
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -18,6 +19,17 @@ object Main {
       .json("data/exemple.json")
 
     df.show(false)
+
+    val transformedDf = addDeeplyNestedColumn(df)
+    transformedDf.printSchema()
+    transformedDf.show(false)
+  }
+
+  // https://furcypin.github.io/spark-frame/use_cases/working_with_nested_data/
+  def addDeeplyNestedColumn(df: DataFrame): DataFrame = {
+  df.withColumn("skills",
+  transform(col("skills"),
+    item => item.withField("skillList", lit("tutu"))))
   }
 }
 
